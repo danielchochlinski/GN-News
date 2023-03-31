83bead590083a181515571c5716dd7f31c431651
Daniel <danielchochlisnki@hotmail.com>
2023-03-31 21:57:59 +0200

    build script

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
-import { useState } from "react";
 import MainTemplate from "./template/MainTemplate";
 import MainView from "./features/Main/MainView";
 import { Route, Routes } from "react-router-dom";
 import CountryView from "./features/Country/CountryView";
 
-const mock = ["1", "2"];
 function App() {
   return (
     <div className="App">
