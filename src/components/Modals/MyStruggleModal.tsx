9b86ca402943ab80109f89df10681cfd7f033ebe
Daniel <danielchochlisnki@hotmail.com>
2023-03-31 21:51:29 +0200

    v

--- src/components/Modals/MyStruggleModal.tsx
+++ src/components/Modals/MyStruggleModal.tsx
@@ -3,9 +3,9 @@ import React from "react";
 export const MyStruggleModal = () => {
   return (
     <>
-      I struggled the most with redux, although i have worked with it a little
-      bit as part of my learning roadmap, I never used it commercialy, for sure
-      i need to do some work on it. <br />
+      I struggled the most with redux, although i have done both as part of my
+      learning roadmap, I never used it commercialy, for sure i need to do some
+      work on it. <br />
       <br />
       So far throughout my commerial expirience compansies have always opted for
       react context therefore i feel very comfortable and fluent with it.
@@ -25,8 +25,8 @@ export const MyStruggleModal = () => {
       Feel free to check it out :)
       <br />
       <br />
-      I also ran out of time a little bit, for my implementation of language
-      support please checkout <br />
+      I also ran out of time, for my implementation of language support please
+      checkout <br />
       <a
         style={{ color: "purple", textDecoration: "underline" }}
         href="www.translogisticscompany.com"
@@ -37,7 +37,9 @@ export const MyStruggleModal = () => {
         TransLogistics Company
       </a>{" "}
       Webiste which i did for one of my freelance clients, i used the
-      react-i18next library.
+      react-i18next library. I also ran out of time to write the tests, if they
+      are essential i will write them in v2 of the project over the weekend,
+      this way you can have a fair idea of how much was done, within the deadline :)
     </>
   );
 };
